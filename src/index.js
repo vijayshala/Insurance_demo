dda47fc612fc068c5d157b042bd4defbfc4f85c1
Keshav Garg <keshav.garg@BRILLIO.COM>
2019-11-12 00:29:29 +0530

    enable cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cors = require('cors');
 require('./db/mongoose')
 const userRouter = require('./routers/user')
  
@@ -6,6 +7,7 @@ const userRouter = require('./routers/user')
 const app = express()
 const port = process.env.PORT || 6000
 
+app.use(cors());
 app.use(express.json())
 app.use(userRouter)
 
